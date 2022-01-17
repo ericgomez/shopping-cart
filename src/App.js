5794b72391f74d79b0fba5241357224c9b98a8b9
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-01-17 15:35:50 -0600

    Add images in the proyect

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+  state = {
+    pruducts: [
+      {
+        name: 'Tomato',
+        price: 1.5,
+        image: '/products/tomato.jpg'
+      },
+      {
+        name: 'Potato',
+        price: 2.5,
+        image: '/products/potato.jpg'
+      },
+      {
+        name: 'Carrot',
+        price: 3.5,
+        image: '/products/carrot.jpg'
+      }
+    ]
+  }
   render () {
     return (
       <div>
