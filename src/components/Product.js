67c71db2aba95a6e81fdc1bd6a58b9d2deb426cb
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-01-17 16:59:45 -0600

    Add styles a the products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,27 @@
 import { Component } from 'react'
 
+const styles = {
+  product: {
+    border: 'solid 1px #eee',
+    boxShadow: '0 5px 5px rgb(0, 0, 0, 0.1)',
+    width: '30%',
+    padding: '10px 15px',
+    borderRadius: '5px'
+  },
+  image: {
+    width: '100%',
+    height: '75%'
+  }
+}
+
 class Product extends Component {
   render () {
     const { product, addCartItem } = this.props
 
-    console.log(this.props)
+    // console.log(this.props)
     return (
-      <div>
-        <img src={product.image} alt={product.name} />
+      <div style={styles.product}>
+        <img src={product.image} alt={product.name} style={styles.image} />
         <h3>{product.name}</h3>
         <p>{product.price}</p>
       </div>
